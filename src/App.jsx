4aad23c971a93ecbe8e380ad1a786e94efcbd797
Yılmaz Emirhan Saraç <yilmazemirhansarac@icloud.com>
2025-07-01 16:58:12 +0300

    Initialize project with Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center p-4">
+        <h1 className="text-2xl font-bold">YESpace</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
